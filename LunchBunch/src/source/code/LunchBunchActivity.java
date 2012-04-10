ada99d011ef63ffcd38d81bf1b7c550d863fbc43
Pallavi P <ppowale@mit.edu>
2012-04-10 00:13:22 -0400

    put project folder

---
+++ LunchBunch/src/source/code/LunchBunchActivity.java
@@ -0,0 +1,13 @@
+package source.code;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LunchBunchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
