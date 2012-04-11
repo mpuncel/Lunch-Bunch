e56bbbb67a698c81183725c4a339195ccebd9fa3
Anjali Muralidhar <amuralid@mit.edu>
2012-04-10 21:58:45 -0400

    added anjalis list shit

---
+++ LunchBunch/src/source/code/LunchInvites.java
@@ -0,0 +1,39 @@
+package source.code;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LunchInvites extends ListActivity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+
+	  setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, COUNTRIES));
+
+	  ListView lv = getListView();
+	  lv.setTextFilterEnabled(true);
+
+	  lv.setOnItemClickListener(new OnItemClickListener() {
+	    public void onItemClick(AdapterView<?> parent, View view,
+	        int position, long id) {
+	      // When clicked, show a toast with the TextView text
+	      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+	          Toast.LENGTH_SHORT).show();
+	    }
+	  });
+	}
+
+	static final String[] COUNTRIES = new String[] {
+	    "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
+	    "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
+	    "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan"};
+}
