071b51ddd1c213b4e9686dd7b9cb03da58020c3b
Pallavi P <ppowale@mit.edu>
2012-04-11 00:40:41 -0400

    added create lunch activity

---
+++ LunchBunch/src/source/code/MyOnItemSelectedListener.java
@@ -0,0 +1,22 @@
+package source.code;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+public class MyOnItemSelectedListener implements OnItemSelectedListener {
+
+
+
+	@Override
+    public void onItemSelected(AdapterView<?> parent,
+            View view, int pos, long id) {
+        }
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
