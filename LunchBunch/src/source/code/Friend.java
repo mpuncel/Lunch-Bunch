5a0a42a0ae456009ac5e8a97b4c607931776dce8
Michael Puncel <mpuncel@mit.edu>
2012-04-11 20:14:42 -0400

    added friend class

---
+++ LunchBunch/src/source/code/Friend.java
@@ -0,0 +1,16 @@
+package source.code;
+
+public class Friend {
+    
+    private String name;
+    
+    public Friend(String n) {
+        this.name = n;
+    }
+    
+    public String toString() {
+        
+        return this.name;
+    }
+
+}
