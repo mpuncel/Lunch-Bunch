4e65766bc0750e215ceeb47423fac6fe7672b5d5
Michael Puncel <mpuncel@mit.edu>
2012-05-03 14:27:55 -0400

    added FriendListAdapter class

---
+++ LunchBunch/src/source/code/FriendListAdapter.java
@@ -0,0 +1,47 @@
+package source.code;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+
+@SuppressWarnings("rawtypes")
+public class FriendListAdapter<T> extends ArrayAdapter {
+    
+    private ArrayList<Boolean> isChecked;
+    private ArrayList<Friend> friends;
+    @SuppressWarnings("unchecked")
+    public FriendListAdapter(Context context, int resource, ArrayList<Friend> friends)
+    {
+        super(context, resource, friends);
+        this.friends = friends;
+        isChecked = new ArrayList<Boolean>(friends.size());
+        for (int i = 0; i < friends.size(); i++) {
+            isChecked.add(i, false);
+        }
+    }
+    
+    public View getView(int position, View convertView, ViewGroup parent) {
+        CheckedTextView v = (CheckedTextView)super.getView(position, convertView, parent);
+        v.setChecked(isChecked.get(position));
+        
+        return v;
+    }
+    
+    public void setChecked(int position, boolean checked) {
+        isChecked.add(position, checked);
+    }
+    
+    public ArrayList<Friend> getSelectedFriends() {
+        ArrayList<Friend> selectedFriends = new ArrayList<Friend>();
+        for (int i = 0; i < this.friends.size(); i++) {
+            if (this.isChecked.get(i) == true) {
+                selectedFriends.add(this.friends.get(i));
+            }
+        }
+        return selectedFriends;
+    }
+}
